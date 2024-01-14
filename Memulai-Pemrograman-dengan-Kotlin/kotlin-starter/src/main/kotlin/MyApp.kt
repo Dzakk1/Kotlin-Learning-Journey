75bf653b32789a35553a77d88d544277908b496c
Dzakk1 <muhadzdzakyzaidan@gmail.com>
2024-01-15 01:00:09 +0700

    reviewing kotlin fundamental

---
+++ Memulai-Pemrograman-dengan-Kotlin/kotlin-starter/src/main/kotlin/MyApp.kt
@@ -0,0 +1,57 @@
+fun main(){
+
+    // TIPE DATA
+    val nama : String = "Arif"
+    var umur = 17
+
+    val firstWord = "Dicoding"
+    val lastWord = "Academy"
+
+    var vocal = 'A'
+
+    val text = "Kotlin"
+    val firstChar = text[0]
+
+
+    println("Hello $nama Umur Saya $umur ")
+    println(firstWord + lastWord)
+
+    println("Vocal " + vocal++)
+    println("Vocal " + vocal++)
+    println("Vocal " + vocal++)
+    println("Vocal " + vocal--)
+    println("Vocal " + vocal--)
+
+    println("First Character of $text is $firstChar")
+
+    for (char in text) {
+        println("$char ")
+    }
+
+    // IF EXPRESSION
+    val openhours = 7
+    val now = 7
+    val office : String
+    office = if (now > 7) {
+        "Office already open"
+    } else if (now == openhours){
+        "Wait a minute, office will be open"
+    } else {
+        "Office is closed"
+    }
+
+    println(office)
+
+    // END EXPRESSION
+    val officeOpen = 7
+    val officeClosed = 16
+    val now1 = 20
+
+    val isOpen = if (now1 >= officeOpen && now1 <= officeClosed){
+        true
+    } else {
+        false
+    }
+
+    println("Office is open : $isOpen")
+}
